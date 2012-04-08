ed4ae8a36d3559de86b581930bc2b74ad08890cc
Masami Yonehara <hdemon7@gmail.com>
2012-04-09 01:01:24 +0900

    Classified.

--- src/content-script.coffee
+++ src/content-script.coffee
@@ -2,9 +2,13 @@ $window = $('window')
 $body = $('body')
 canvas = null
 $canvas = null
-canvas_context = null
+original_canvas = null
+original_canvas_context = null
+canvas_context = []
 context = []
 piece = []
+id_total = 0
+length_of_side = 10
 
 $window.ready(()->
   $body.html2canvas({
@@ -16,26 +20,26 @@ $window.ready(()->
 
 initialize = () ->
   $window.unbind()
-  render()
-  divide_into_pieces(10)
+  original_canvas = new Original()
+  original_canvas.divide_into_pieces(length_of_side)
+  get_all_contents()
 
-render = () ->
-  $canvas
-    .css({
-      position: 'absolute', 
-      left: 0, 
-      top: 0 })
-    .appendTo(document.body)
-    .siblings()
-    .toggle()
-  $body.append(canvas)
 
-get_canvas_context = () ->
+get_original_canvas_context = () ->
   document.getElementsByTagName('canvas')[0].getContext('2d')
 
+get_canvas_context = (id) ->
+  document.getElementById("tmp_#{id}").getContext('2d')
+
+get_all_contents = () ->
+  for id in id_total
+    do (id) ->
+      canvas_context[id] = get_canvas_context(id)
+
+
 image_data = (x, y, length_of_side) ->
-  canvas_context ||= get_canvas_context()
-  canvas_context.getImageData(x, y, length_of_side, length_of_side)
+  original_canvas_context ||= get_original_canvas_context()
+  original_canvas_context.getImageData(x, y, length_of_side, length_of_side)
 
 divide_into_pieces = (length_of_side) ->
   temp_canvas = []
@@ -48,6 +52,7 @@ divide_into_pieces = (length_of_side) ->
         id = id + 1;
       }
     }
+    id_total = id
   };`
 
   itelator((id) ->
@@ -56,29 +61,68 @@ divide_into_pieces = (length_of_side) ->
   $body.append(temp_canvas.join(""))
 
   itelator((id, x, y) ->
-    piece = document.getElementById("tmp_#{id}")
-    context = piece.getContext('2d')
-    context.putImageData(image_data(x, y, length_of_side), 0, 0)
+    piece[id] = new Piece(image_data, id, x, y)
+  )
+
+class Original
+  constructor: () ->
+    this.render()
+    this.context = document.getElementsByTagName('canvas')[0].getContext('2d')
+
+  render: () ->
+    $canvas
+      .css({
+        position: 'absolute', 
+        left: 0, 
+        top: 0 })
+      .appendTo(document.body)
+      .siblings()
+      .toggle()
+    $body.append(canvas)
+
+  get_content: () ->
+    document.getElementsByTagName('canvas')[0].getContext('2d')
+
+  divide_into_pieces: () ->
+    temp_canvas = []
+
+    itelator = `function(callback) {
+      var id = 0;
+      for (var y = 0, max_y = $canvas.height(); y < max_y; y = y + length_of_side) {
+        for (var x = 0, max_x = $canvas.width(); x < max_x; x = x + length_of_side) {
+          callback(id, x, y);
+          id = id + 1;
+        }
+      }
+      id_total = id
+    };`
+
+    itelator((id) ->
+      temp_canvas.push("<canvas id=\"tmp_#{id}\" width=\"#{length_of_side}\" height=\"#{length_of_side}\" />")
+    )
+    $body.append(temp_canvas.join(""))
+
+    itelator((id, x, y) ->
+      piece[id] = new Piece(this.image_data, id, x, y)
+    )
+
+  image_data: (x, y) ->
+    this.context.getImageData(x, y, length_of_side, length_of_side)
+
+class Piece
+  length_of_side: length_of_side
+  constructor: (@image_data, @id, @x, @y) ->
+    this.rotating_angle = 0
+    this.shooted = false
+    this.context = document.getElementById("tmp_#{this.id}").getContext('2d')
+
+    this.context.putImageData(original_canvas.image_data(x, y, length_of_side), 0, 0)
     $(piece)
       .css({
         position: "absolute",
         left: x,
         top:  y
       })
-    context.delete
-  )
-
-
-class Piece
-  constructor: (@image_data, x, y) ->
-    this.x = x
-    this.y = y
-    this.rotating_angle = 0
-    this.shooted = false
-
-    $("<canvas id=\"tmp_#{id}\">")
-      .appendTo(document.body)
-    context = document.getElementsById("tmp_#{id}")[0].getContext('2d')
 
   render: () ->
     canvas_context ||= canvas_context()
--- src/content-script.js
+++ src/content-script.js
@@ -1,5 +1,5 @@
 (function() {
-  var $body, $canvas, $window, Piece, canvas, canvas_context, context, divide_into_pieces, get_canvas_context, image_data, initialize, piece, render;
+  var $body, $canvas, $window, Original, Piece, canvas, canvas_context, context, divide_into_pieces, get_all_contents, get_canvas_context, get_original_canvas_context, id_total, image_data, initialize, length_of_side, original_canvas, original_canvas_context, piece;
 
   $window = $('window');
 
@@ -9,11 +9,19 @@
 
   $canvas = null;
 
-  canvas_context = null;
+  original_canvas = null;
+
+  original_canvas_context = null;
+
+  canvas_context = [];
 
   context = [];
 
   piece = [];
+
+  id_total = 0;
+
+  length_of_side = 10;
 
   $window.ready(function() {
     return $body.html2canvas({
@@ -26,26 +34,34 @@
 
   initialize = function() {
     $window.unbind();
-    render();
-    return divide_into_pieces(10);
+    original_canvas = new Original();
+    original_canvas.divide_into_pieces(length_of_side);
+    return get_all_contents();
   };
 
-  render = function() {
-    $canvas.css({
-      position: 'absolute',
-      left: 0,
-      top: 0
-    }).appendTo(document.body).siblings().toggle();
-    return $body.append(canvas);
-  };
-
-  get_canvas_context = function() {
+  get_original_canvas_context = function() {
     return document.getElementsByTagName('canvas')[0].getContext('2d');
   };
 
+  get_canvas_context = function(id) {
+    return document.getElementById("tmp_" + id).getContext('2d');
+  };
+
+  get_all_contents = function() {
+    var id, _i, _len, _results;
+    _results = [];
+    for (_i = 0, _len = id_total.length; _i < _len; _i++) {
+      id = id_total[_i];
+      _results.push((function(id) {
+        return canvas_context[id] = get_canvas_context(id);
+      })(id));
+    }
+    return _results;
+  };
+
   image_data = function(x, y, length_of_side) {
-    canvas_context || (canvas_context = get_canvas_context());
-    return canvas_context.getImageData(x, y, length_of_side, length_of_side);
+    original_canvas_context || (original_canvas_context = get_original_canvas_context());
+    return original_canvas_context.getImageData(x, y, length_of_side, length_of_side);
   };
 
   divide_into_pieces = function(length_of_side) {
@@ -59,34 +75,85 @@
         id = id + 1;
       }
     }
+    id_total = id
   };;
     itelator(function(id) {
       return temp_canvas.push("<canvas id=\"tmp_" + id + "\" width=\"" + length_of_side + "\" height=\"" + length_of_side + "\" />");
     });
     $body.append(temp_canvas.join(""));
     return itelator(function(id, x, y) {
-      piece = document.getElementById("tmp_" + id);
-      context = piece.getContext('2d');
-      context.putImageData(image_data(x, y, length_of_side), 0, 0);
+      return piece[id] = new Piece(image_data, id, x, y);
+    });
+  };
+
+  Original = (function() {
+
+    function Original() {
+      this.render();
+      this.context = document.getElementsByTagName('canvas')[0].getContext('2d');
+    }
+
+    Original.prototype.render = function() {
+      $canvas.css({
+        position: 'absolute',
+        left: 0,
+        top: 0
+      }).appendTo(document.body).siblings().toggle();
+      return $body.append(canvas);
+    };
+
+    Original.prototype.get_content = function() {
+      return document.getElementsByTagName('canvas')[0].getContext('2d');
+    };
+
+    Original.prototype.divide_into_pieces = function() {
+      var itelator, temp_canvas;
+      temp_canvas = [];
+      itelator = function(callback) {
+      var id = 0;
+      for (var y = 0, max_y = $canvas.height(); y < max_y; y = y + length_of_side) {
+        for (var x = 0, max_x = $canvas.width(); x < max_x; x = x + length_of_side) {
+          callback(id, x, y);
+          id = id + 1;
+        }
+      }
+      id_total = id
+    };;
+      itelator(function(id) {
+        return temp_canvas.push("<canvas id=\"tmp_" + id + "\" width=\"" + length_of_side + "\" height=\"" + length_of_side + "\" />");
+      });
+      $body.append(temp_canvas.join(""));
+      return itelator(function(id, x, y) {
+        return piece[id] = new Piece(this.image_data, id, x, y);
+      });
+    };
+
+    Original.prototype.image_data = function(x, y) {
+      return this.context.getImageData(x, y, length_of_side, length_of_side);
+    };
+
+    return Original;
+
+  })();
+
+  Piece = (function() {
+
+    Piece.prototype.length_of_side = length_of_side;
+
+    function Piece(image_data, id, x, y) {
+      this.image_data = image_data;
+      this.id = id;
+      this.x = x;
+      this.y = y;
+      this.rotating_angle = 0;
+      this.shooted = false;
+      this.context = document.getElementById("tmp_" + this.id).getContext('2d');
+      this.context.putImageData(original_canvas.image_data(x, y, length_of_side), 0, 0);
       $(piece).css({
         position: "absolute",
         left: x,
         top: y
       });
-      return context["delete"];
-    });
-  };
-
-  Piece = (function() {
-
-    function Piece(image_data, x, y) {
-      this.image_data = image_data;
-      this.x = x;
-      this.y = y;
-      this.rotating_angle = 0;
-      this.shooted = false;
-      $("<canvas id=\"tmp_" + id + "\">").appendTo(document.body);
-      context = document.getElementsById("tmp_" + id)[0].getContext('2d');
     }
 
     Piece.prototype.render = function() {
